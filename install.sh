e9fb32fe306488db62bc4f3fe7f82e5af1d6fa4c
itinglight <itinglight@gmail.com>
2021-09-19 10:42:22 +0800

    add vim plugin

--- install.sh
+++ install.sh
@@ -1,2 +1,10 @@
+rm ~/.vimrc
+rm ~/.zshrc
 ln ./vimrc ~/.vimrc
-# ln ./zshrc ~/.zshrc
+ln ./zshrc ~/.zshrc
+
+
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+	
